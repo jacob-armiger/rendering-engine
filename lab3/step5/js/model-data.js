068a89a11f709587c34b80c4f6f57e3c17012b56
jacob-armiger <armigersemail@gmail.com>
2023-04-05 21:39:32 -0400

    Update lab3/step5/

--- lab3/step5/js/model-data.js
+++ lab3/step5/js/model-data.js
@@ -1,8 +1,13 @@
 class Shape {
   constructor() {
-    this.textureIMG = ""
+    // Sources
     this.objDataPath = ""
+    this.textureParams = {
+      src: null,
+      type: null,
+    }
 
+    // World translation data
     this.position = [0,0,0]
     this.scaleVector = [4,4,4]
     this.boundingVector = [1,1,1]
@@ -11,22 +16,17 @@ class Shape {
     this.roationDegree = 10
     this.rotateOnTime = false
 
+    // Info for shape's shader program
+    this.shaderVertSrc = null
+    this.shaderFragSrc = null
     this.modelViewMatrix = null
 
+    // Maintain instance of shader prgram
     this.shaderProgram = null
-    this.bufferMap = null
-    this.parsedData = null
 
+    // Maintain instance of drawable
     this.myDrawable = null
     this.drawableInitialized = false
-  }
-
-  setTexImg(src) {
-    this.textureIMG = src
-  }
-
-  setObjDataPath(objFile) {
-    this.objDataPath = "../../shared/resources/models/" + objFile
   }
 
   setRotationValues(axis, degree, onTime) {
@@ -43,13 +43,20 @@ class Shape {
     this.position = [x,y,z]
   }
 
-  setUseCubemap() {
-    this.useCubemap = true
+  setTexParams(src, type) {
+    this.textureParams.src = src
+    this.textureParams.type = type
   }
 
-  getUsingCubemap() {
-    return this.useCubemap
+  setObjDataPath(objFile) {
+    this.objDataPath = "../../shared/resources/models/" + objFile
   }
+
+  setShaderSrc(shaderName) {
+    this.shaderVertSrc = `../../shared/resources/shaders/verts/${shaderName}.vert`
+    this.shaderFragSrc = `../../shared/resources/shaders/frags/${shaderName}.frag`
+  }
+
 }
 
 
@@ -58,6 +65,8 @@ function createShapeData() {
 
   let floor = new Shape();
   floor.setObjDataPath("floor.obj");
+  floor.setShaderSrc("texturePhong")
+  floor.setTexParams("sidewalk.jpg", "image")
 
   floor.setPositionValue(0, -1.3, 0);
   floor.setScaleValue(20);
@@ -67,17 +76,29 @@ function createShapeData() {
 
   let ball = new Shape();
   ball.setObjDataPath("sphere_with_vt.obj");
+  ball.setShaderSrc("texturePhong")
+  ball.setTexParams("hd_power_t.png", "image")
 
-  ball.setRotationValues([1, 1, 0], 0, true);
-  ball.setPositionValue(3, 0, 0);
+  ball.setRotationValues([0, -1, 0], 0, true);
+  ball.setPositionValue(2, 1, 4);
   list.push(ball)
 
   let cube = new Shape()
   cube.setObjDataPath("box_with_vt.obj");
+  cube.setShaderSrc("textureCubemap")
+  cube.setTexParams("coit_tower/", "cubemap")
 
-  // cube.setRotationValues([1, 1, 0], 0, true);
-  cube.setPositionValue(0,0,0)
+  cube.setRotationValues([1, 1, 0], 0, true);
+  cube.setPositionValue(-2,7,-6)
   list.push(cube)
+
+  let cube2 = new Shape()
+  cube2.setObjDataPath("box_with_vt.obj");
+  cube2.setShaderSrc("phong300")
+
+  cube2.setRotationValues([0, 1, 0], -0.5, false);
+  cube2.setPositionValue(-2,0,0)
+  list.push(cube2)
 
   return list;
 }
