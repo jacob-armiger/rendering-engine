32f2260177f58a96b96436a3c4b24ea964e0a0e7
jacob-armiger <armigersemail@gmail.com>
2023-04-19 14:48:51 -0400

    Add dynamic normalmap file selection

--- midterm/js/model-data.js
+++ midterm/js/model-data.js
@@ -51,12 +51,12 @@ class Shape {
   }
 
   setObjDataPath(objFile) {
-    this.objDataPath = "../../shared/resources/models/" + objFile
+    this.objDataPath = "../shared/resources/models/" + objFile
   }
 
   setShaderSrc(shaderName) {
-    this.shaderVertSrc = `../../shared/resources/shaders/verts/${shaderName}.vert`
-    this.shaderFragSrc = `../../shared/resources/shaders/frags/${shaderName}.frag`
+    this.shaderVertSrc = `../shared/resources/shaders/verts/${shaderName}.vert`
+    this.shaderFragSrc = `../shared/resources/shaders/frags/${shaderName}.frag`
   }
 
 }
@@ -65,79 +65,13 @@ class Shape {
 function createShapeData() {
   list = [];
 
-  // let floor = new Shape();
-  // floor.setObjDataPath("floor.obj");
-  // floor.setShaderSrc("texturePhong")
-  // floor.setTexParams("sidewalk.jpg", "image")
-
-  // floor.setPositionValue(0, -1.3, 0);
-  // floor.setScaleValue(20);
-  // floor.setRotationValues([1, 0, 0], -1.55, false);
-
-  // list.push(floor)
-
-  // let ball = new Shape();
-  // ball.setObjDataPath("sphere_with_vt.obj");
-  // ball.setShaderSrc("texturePhong")
-  // ball.setTexParams("sidewalk.jpg", "image")
-
-  // // ball.setRotationValues([0, 0, 0], 0, true);
-  // ball.setPositionValue(4, 0, 0);
-  // list.push(ball)
-
-  // let top = new Shape();
-  // top.setObjDataPath("box_with_vt.obj");
-  // top.setShaderSrc("texturePhong")
-  // top.setTexParams("hd_power_t.png", "image")
-
-  // top.setRotationValues([0, 0, 0], 0, true);
-  // top.setPositionValue(0, 6, 0);
-  // list.push(top)
-
-  // let front = new Shape();
-  // front.setObjDataPath("sphere_with_vt.obj");
-  // front.setShaderSrc("texturePhong")
-  // front.setTexParams("sidewalk.jpg", "image")
-
-  // front.setRotationValues([0, 0, 0], 0, true);
-  // front.setPositionValue(0, 2, -1);
-  // list.push(front)
-
-  // let ball2 = new Shape();
-  // ball2.setObjDataPath("sphere_with_vt.obj");
-  // ball2.setShaderSrc("textureCubemap")
-  // ball2.setTexParams("coit_tower/", "cubemap")
-
-  // ball2.setRotationValues([0, 0, 0], 0, true);
-  // ball2.setPositionValue(4, 3, 0);
-  // list.push(ball2)
-
-  // let cube = new Shape()
-  // cube.setObjDataPath("sphere_with_vt.obj");
-  // cube.setShaderSrc("textureCubemap")
-  // cube.setTexParams(null, "dynamicCubemap")
-  
-  // cube.setRotationValues([0, 1, 0], 0, false);
-  // cube.setPositionValue(0,0,0)
-  // list.push(cube)
-
   let cube2 = new Shape()
   cube2.setObjDataPath("box_with_vt.obj");
   cube2.setShaderSrc("textureNormMap")
-  cube2.setTexParams("hd_wood.png", "normalmap")
-
+  cube2.setTexParams("toy_box", "normalmap")
   // cube2.setRotationValues([0, 1, 0], 0, true);
   cube2.setPositionValue(0,1,0)
   list.push(cube2)
-
-  // let cube3 = new Shape()
-  // cube3.setObjDataPath("box_with_vt.obj");
-  // cube3.setShaderSrc("texturePhong")
-  // cube3.setTexParams("sidewalk.jpg", "image")
-  
-  // cube3.setRotationValues([0, 1, 0], 0, true);
-  // cube3.setPositionValue(-4,0,0)
-  // list.push(cube3)
 
   return list;
 }
